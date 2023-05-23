89ea9ba22bc133be38ead8f74660b96d91cd4590
yourname <youremail>
2023-05-23 18:13:49 +0900

    stripe checkout

--- pages/token-topup.js
+++ pages/token-topup.js
@@ -1,12 +1,15 @@
 import { withPageAuthRequired } from "@auth0/nextjs-auth0";
 import { AppLayout } from "../components/AppLayout";
-import getAppProps from "../utils/getAppProps";
+import { getAppProps } from "../utils/getAppProps";
 
 export default function TokenTopup() {
   const handleClick = async () => {
-    await fetch(`/api/addTokens`, {
+    const result = await fetch(`/api/addTokens`, {
       method: "POST",
     });
+    const json = await result.json();
+    console.log("RESULT: ", json);
+    window.location.href = json.session.url;
   };
 
   return (
